cc108a3353186d2e587b3ad460c4c64cac3019ad
Ansel-Kee <a1.kee.jx@gmail.com>
2025-08-30 02:28:24 +0800

    reorganised and added basic flask app

---
+++ app.py
@@ -0,0 +1,28 @@
+# app.py
+from flask import Flask, render_template, request
+from flask import jsonify
+import json
+
+app = Flask(__name__, template_folder='templates')
+
+# Display your index page
+@app.route("/")
+def index():
+    return render_template("/index.html")
+# A function to add two numbers
+@app.route("/model/<batch>", methods=["POST"])
+def analyse(batch: str):
+    if batch == "batch":
+        data = request
+        print(data.get_json())
+        
+    else:
+        review = request.args.get("review")
+        location = request.args.get("location")
+        data = [[review]]
+    # result = model.analyse(data)
+    result = {}
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
