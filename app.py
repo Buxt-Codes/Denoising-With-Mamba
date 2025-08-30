947b4270b60dc940c634bb496124a7b07e835f6a
Ansel-Kee <a1.kee.jx@gmail.com>
2025-08-31 00:12:58 +0800

    updated to @rabbitseating's new index.html and edited to re add flask compatibility

--- app.py
+++ app.py
@@ -10,18 +10,62 @@ app = Flask(__name__, template_folder='templates')
 def index():
     return render_template("/index.html")
 # A function to add two numbers
-@app.route("/model/<batch>", methods=["POST"])
+@app.route("/api/<batch>", methods=["POST"])
 def analyse(batch: str):
-    if batch == "batch":
+    if batch == "analyze-batch":
         data = request.get_json()
+        print(data)
         
     else:
         review = request.args.get("review")
         location = request.args.get("location")
         data = [[review]]
     # result = model.analyse(data)
-    result = {}
-    return jsonify(result)
+    result = """[
+  {
+    "location": "macs",
+    "review": "Not that good",
+    "cf": 0.1,
+    "label": 1
+  },
+  {
+    "location": "macs",
+    "review": "that",
+    "cf": 0.645,
+    "label": 0
+  },
+  {
+    "location": "macs",
+    "review": "good",
+    "cf": 0.1,
+    "label": 1
+  },
+  {
+    "location": "macs",
+    "review": "Not it",
+    "cf": 0.4,
+    "label": 0
+  },
+  {
+    "location": "macs",
+    "review": "Not it good",
+    "cf": 0.6,
+    "label": 0
+  },
+  {
+    "location": "macs",
+    "review": "Not that",
+    "cf": 0.9,
+    "label": 0
+  },
+  {
+    "location": "macs",
+    "review": "Not",
+    "cf": 0.04933643634664364,
+    "label": 1
+  }
+]"""
+    return result
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
